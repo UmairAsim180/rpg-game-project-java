687df5ae2f7d846cf0d569ff9ea6feea053f3960
umairrasheed353-glitch <umairrasheed353@gmail.com>
2026-05-15 23:00:15 +0500

    Practise Files Deleted

--- src/main/java/com/example/final_project/GithubPractise.java
+++
@@ -1,5 +0,0 @@
-package com.example.final_project;
-
-public class GithubPractise {
-    //hlo this will be uploaded to Github Soon
-}
--- src/main/java/com/example/final_project/Practice.java
+++
@@ -1,9 +0,0 @@
-package com.example.final_project;
-
-public class Practice {
-    static void main(String[] args) {
-        System.out.println("practicing");
-    }
-
-
-}
