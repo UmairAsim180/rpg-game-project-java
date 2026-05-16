e84490f29e969cdd6fca423ecb6d1fad9151ae68
Umair Asim <umairasim0598@gmail.com>
2026-05-16 07:29:21 +0500

    Program Not Running Problem Fixed

--- src/main/java/com/example/final_project/StartScreenController.java
+++ src/main/java/com/example/final_project/StartScreenController.java
@@ -1,5 +1,14 @@
 package com.example.final_project;
 
+import javafx.fxml.FXML;
+
 public class StartScreenController {
-    
+    @FXML
+    public void onClickStart(){
+        System.out.println("Click game start");
+    }
+    @FXML
+    public void onExitClick(){
+        System.out.println("Click game Exit");
+    }
 }
