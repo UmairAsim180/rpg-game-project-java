cc547480b652721518c736011fd10afc0694a745
umairrasheed353-glitch <umairrasheed353@gmail.com>
2026-05-16 09:21:51 +0500

    Model Classes Added

---
+++ src/main/java/models/BossEnemy.java
@@ -0,0 +1,57 @@
+package models;
+
+public class BossEnemy extends Enemy{
+    private int phase;
+    private String specialAbility;
+    private int turnCounter;
+
+    public BossEnemy(String name, String specialAbility) {
+        super(name, "Boss", 200, 22, 100);
+        this.phase = 1;
+        this.specialAbility = specialAbility;
+        this.turnCounter = 0;
+    }
+
+    public int heavyAttack() {
+        int randomBonus = (int) (Math.random() * 10);
+        return damage + randomBonus + (phase * 5);
+    }
+    public int specialAttack() {
+        return (damage * 2) + (int)(Math.random() * 8);
+    }
+    @Override
+    public int chooseAction(){
+        turnCounter++;
+
+        if (turnCounter % 3 == 0){
+            return specialAttack();
+        }
+        return heavyAttack();
+    }
+    public boolean checkPhaseChange(){
+        if (phase == 1 && currentHealth <= maxHealth / 2){
+            changePhase();
+            return true;
+        }
+        return false;
+    }
+    public void changePhase(){
+        phase = 2;
+        damage += 10;
+    }
+    public boolean isSpecialAttackTurn(){
+        return (turnCounter + 1) % 3 == 0;
+    }
+
+    public int getPhase() {
+        return phase;
+    }
+
+    public String getSpecialAbility() {
+        return specialAbility;
+    }
+
+    public int getTurnCounter() {
+        return turnCounter;
+    }
+}
